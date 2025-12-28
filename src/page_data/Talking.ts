04178a00733a027ab0f5b2f9f64d680190ab045b
zlm6666 <111101880+zlm6666@users.noreply.github.com>
2025-12-28 17:53:36 +0800

    Update Talking.ts

--- src/page_data/Talking.ts
+++ src/page_data/Talking.ts
@@ -1,6 +1,6 @@
 export default {
   // API 接口请求优先，数据格式保持和 data 一致
-  api: '',
+  api: 'https://xiao313.dpdns.org/.netlify/functions/talking',
   // api 为空则使用 data 静态数据 
   // 注意：图片请用 vh-img-flex 类包裹
    data: [
