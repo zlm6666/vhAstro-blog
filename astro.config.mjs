3f7c362c43f0ab8e286193ad2c224cadb8789755
zlm6666 <111101880+zlm6666@users.noreply.github.com>
2026-01-01 20:17:34 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -19,7 +19,6 @@ import swup from '@swup/astro';
 export default defineConfig({
 	site: SITE_INFO.Site,
 	build: { assets: 'vh_static' },
-	trailingSlash: 'never',
 	integrations: [swup({
 		theme: false,
 		animationClass: "vh-animation-",
