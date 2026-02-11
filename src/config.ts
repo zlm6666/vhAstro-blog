440d35c4a6f8f172c576eb3b532717e6b1303347
zlm6666 <111101880+zlm6666@users.noreply.github.com>
2026-02-11 17:54:59 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -105,7 +105,7 @@ export default {
     }
   },
   // Han Analytics 统计（https://github.com/uxiaohan/HanAnalytics）
-  HanAnalytics: { enable: false, server: 'https://analytics.vvhan.com', siteId: 'Hello-HanHexoBlog' },
+  HanAnalytics: { enable: true, server: 'https://fenxi.xiaow.qzz.io', siteId: 'xiao-blog' },
   // Google 广告
   GoogleAds: {
     ad_Client: '', //ca-pub-xxxxxx
